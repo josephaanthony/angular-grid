0694acc7658e065530746ae362f7b11cac4fb60a
josephaanthony <josephaanthony@gmail.com>
2018-08-11 18:42:11 -0400

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,14 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild } from "@angular/core";
+import { HttpClient } from "@angular/common/http";
+import "ag-grid-enterprise";
+import { ParamsRendererComponent } from "./ag-grid/params-renderer.component";
+import { DropDownRendererComponent } from "./ag-grid/dropdown-renderer.component";
+declare var MockServer;
+declare var ViewportDatasource;
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  selector: "app-root",
+  templateUrl: 'app.component.html'
 })
 export class AppComponent {
-  title = 'angular-grid';
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,61 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { HttpClientModule } from "@angular/common/http";
+import { FormsModule, ReactiveFormsModule } from "@angular/forms"; // <-- NgModel lives here
+import { RouterModule, Routes } from '@angular/router';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { DropDownsModule } from '@progress/kendo-angular-dropdowns';
+//import { DialogModule } from '@progress/kendo-angular-dialog';
+import { VirtualScrollModule } from 'angular2-virtual-scroll';
 import { AppComponent } from './app.component';
+import { AgGridModule } from 'ag-grid-angular';
+import { ParamsRendererComponent } from './ag-grid/params-renderer.component';
+import { DropDownRendererComponent } from './ag-grid/dropdown-renderer.component';
+import { AgGridComponent } from './ag-grid/ag-grid.component';
+import { KendoGridComponent } from './kendo-grid/kendo-grid.component';
+import { GridModule } from '@progress/kendo-angular-grid';
+import { EditService } from './kendo-grid/edit.service';
+import { InputComponent } from './shared/input-component/input.component';
+import { DropDownComponent } from './shared/dropdown-component/dropdown.component';
+import { ExtraComponent } from './shared/extra-component/extra.component';
+//import 'ag-grid-enterprise';
+
+const appRoutes: Routes = [
+  { path: 'ag-grid', component: AgGridComponent },
+  { path: 'kendo-grid',      component: KendoGridComponent },
+  { path: '', component: AppComponent }
+];
 
 @NgModule({
   declarations: [
+    ParamsRendererComponent,
+    DropDownRendererComponent,
+    KendoGridComponent,
+    AgGridComponent,
+    InputComponent,
+    DropDownComponent,
+    ExtraComponent,
     AppComponent
   ],
   imports: [
-    BrowserModule
+    RouterModule.forRoot(
+      appRoutes,
+      { enableTracing: true } // <-- debugging purposes only
+    ),
+    BrowserModule,
+    BrowserAnimationsModule,
+    ReactiveFormsModule,
+    FormsModule,
+    DropDownsModule,
+    VirtualScrollModule,
+    //DialogModule,
+    HttpClientModule,
+    GridModule,
+    AgGridModule.withComponents([ParamsRendererComponent, DropDownRendererComponent])
   ],
-  providers: [],
+  providers: [EditService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
+
+
